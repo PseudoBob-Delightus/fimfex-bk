eb5b227ea95895cad6f80497294c0aedfe093913
Silk Rose <silkrose@love-tolerance.com>
2024-09-21 00:49:48 -0400

    Finish create exchange, add generate passphrase

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use pony::fs::find_files_in_dir;
+use rand::Rng;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -8,11 +9,11 @@ use std::path::Path;
 use std::sync::{Arc, Mutex};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Exchangetitle {
+struct ExchangeTitle {
 	title: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Exchange {
 	title: String,
 	id: i32,
@@ -23,7 +24,7 @@ struct Exchange {
 	results: Vec<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 enum Stage {
 	Submission,
 	Voting,
@@ -31,10 +32,30 @@ enum Stage {
 }
 
 #[post("/create-exchange")]
-async fn create_exchange(item: web::Query<Exchangetitle>) -> impl Responder {
-	let new_item = item.into_inner();
-	println!("{:#?}", new_item);
-	HttpResponse::Ok().json(new_item)
+async fn create_exchange(
+	title: web::Query<ExchangeTitle>, data: web::Data<Arc<Mutex<HashMap<i32, Exchange>>>>,
+) -> Result<impl Responder, Box<dyn std::error::Error>> {
+	let title = title.into_inner().title;
+	let mut exchanges = data.lock().unwrap();
+	let id = if exchanges.is_empty() {
+		1
+	} else {
+		exchanges.clone().into_keys().max().unwrap() + 1
+	};
+	let exchange = Exchange {
+		title: title.clone(),
+		id,
+		token: generate_passphrase(),
+		stage: Stage::Submission,
+		submissions: HashMap::new(),
+		votes: HashMap::new(),
+		results: Vec::new(),
+	};
+	exchanges.insert(id, exchange.clone());
+	let path = format!("./exchanges/{id}.json");
+	let contents = serde_json::to_string_pretty(&exchange)?;
+	fs::write(path, contents)?;
+	Ok(HttpResponse::Created().json(exchange))
 }
 
 #[actix_web::main]
@@ -63,4 +84,62 @@ async fn main() -> std::io::Result<()> {
 	.bind(("127.0.0.1", 7669))?
 	.run()
 	.await
+}
+
+fn generate_passphrase() -> String {
+	let characters = [
+		"TwilightSparkle",
+		"Rarity",
+		"PinkiePie",
+		"Fluttershy",
+		"RainbowDash",
+		"Applejack",
+		"MayorMare",
+		"DoctorWhooves",
+		"Derpy",
+		"Coloratura",
+		"DaringDo",
+		"PhotoFinish",
+		"FancyPants",
+		"SapphireShores",
+		"Spitfire",
+		"Soarin",
+		"SunnyStarscout",
+		"IzzyMoonbow",
+		"QueenChrysalis",
+		"SilkRose",
+	];
+	let actions = [
+		"Boops",
+		"Kisses",
+		"Likes",
+		"Loves",
+		"WantsToDate",
+		"Hugs",
+		"Holds",
+		"TalksTo",
+		"SingsTo",
+		"AsksOut",
+		"GivesABitTo",
+		"HasCoffeeWith",
+		"HasAVerySpecificQuestionFor",
+		"IsNotTheSamePonyAs",
+		"HasACrushOn",
+		"IsMarrying",
+		"TripsInFrontOf",
+		"LooksAt",
+		"HoldsHoovesWith",
+	];
+
+	let mut rng = rand::thread_rng();
+
+	let char1 = characters[rng.gen_range(0..characters.len())];
+	let char2 = characters[rng.gen_range(0..characters.len())];
+
+	if char1 == char2 {
+		format!("{char1}IsTheSamePonyAs{char2}")
+	} else {
+		let action = actions[rng.gen_range(0..actions.len())];
+		format!("{char1}{action}{char2}")
+	}
 }
